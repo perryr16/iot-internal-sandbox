7fca45125e082e86ab31294ae6c1d99ccd1a036c
Ross Perry <perryr16@gmail.com>
2021-10-26 12:44:31 -0600

    basic layout functional

--- src/components/office/OfficeFloor.tsx
+++ src/components/office/OfficeFloor.tsx
@@ -12,176 +12,176 @@ export const OfficeFloor: React.FC<Props> = () => {
 
 
     return (
-        <div className='office-floor-content grid' style={{border: '1px solid yellow'}}>
-            <img className='floor-plan' src={floorPlan} alt="office-floor-plan" style={{border: '1px solid blue'}}/>
+        <div className='office-floor-content grid'>
+            <img className='floor-plan' src={floorPlan} alt="office-floor-plan" />
             {/* col 1 */}
             <Columnx
                 child3={
                     <Sensor 
                         page={'Line'}
-                        info={'Sensor: H20 \n Type: SensManf'}
+                        info={'Sensor: H20 \n Type: ABC Sensor \n Graph: Line'}
                     />
                 }
-                child4={
-                    <Sensor 
-                        page={'Line'}
-                        info={'Sensor: H20 \n Type: SensManf'}
-                    />
-                }
+                // child4={
+                //     <Sensor 
+                //         page={'Line'}
+                //         info={'Sensor: H20 \n Type: ABC Sensor \n Graph: Line'}
+                //     />
+                // }
             />
             {/* col 2 */}
             <Columnx
-                child2={
+                // child2={
+                //     <Sensor 
+                //         page={'Line'}
+                //         info={'Sensor: H20 \n Type: ABC Sensor \n Graph: Line'}
+                //     />
+                // }
+                child5={
                     <Sensor 
-                        page={'Line'}
-                        info={'Sensor: H20 \n Type: SensManf'}
-                    />
-                }
-                child4={
-                    <Sensor 
-                        page={'Line'}
-                        info={'Sensor: H20 \n Type: SensManf'}
+                        page={'Pie'}
+                        info={'Sensor: H20 \n Type: ABC Sensor \n Graph: Pie'}
                     />
                 }
             />
             {/* col 3 */}
             <Columnx
-                child2={
-                    <Sensor 
-                        page={'Line'} 
-                        info={'Sensor: Motion \n Type: SensManf'}
-                    />}
-                child4={
-                    <Sensor 
-                        page={'Line'}
-                        info={'Sensor: Motion \n Type: SensManf'}
-                        faceNorthEast={true}
-                    />}
+                // child2={
+                //     <Sensor 
+                //         page={'Line'} 
+                //         info={'Sensor: Motion \n Type: ABC Sensor \n Graph: Line'}
+                //     />}
+                // child4={
+                //     <Sensor 
+                //         page={'Line'}
+                //         info={'Sensor: Motion \n Type: ABC Sensor \n Graph: Line'}
+                //         faceNorthEast={true}
+                //     />}
                 />
             {/* col 4 */}
             <Columnx
                 child1={
                     <Sensor 
                         page={'Line'} 
-                        info={'Sensor: CO2 \n Type: SensManf'}
+                        info={'Sensor: CO2 \n Type: ABC Sensor \n Graph: Line'}
                         faceNorthEast={true}
                     />
                 }
-                child4={
+                child3={
                     <Sensor 
-                        page={'Line'}
-                        info={'Sensor: CO2 \n Type: SensManf'}
+                        page={'Bar'}
+                        info={'Sensor: CO2 \n Type: ABC Sensor \n Graph: Bar'}
                         faceNorthEast={true}
                     />
                 }
             />
             {/* col 5 */}
             <Columnx
-                child2={
-                    <Sensor 
-                        page={'Line'} 
-                        info={'Sensor: Motion \n Type: SensManf'}
-                        faceNorthEast={true}
-                    />
-                }
-                child5={
-                    <Sensor 
-                        page={'Line'}
-                        info={'Sensor: Temperature \n Type: SensManf'}
-                        faceNorthEast={true}
-                    />
-                }
+                // child2={
+                //     <Sensor 
+                //         page={'Line'} 
+                //         info={'Sensor: Motion \n Type: ABC Sensor \n Graph: Line'}
+                //         faceNorthEast={true}
+                //     />
+                // }
+                // child5={
+                //     <Sensor 
+                //         page={'Line'}
+                //         info={'Sensor: Temperature \n Type: ABC Sensor \n Graph: Line'}
+                //         faceNorthEast={true}
+                //     />
+                // }
             />
             {/* col 6 */}
             <Columnx
-                child2={
-                    <Sensor 
-                        page={'Line'} 
-                        info={'Sensor: CO2 \n Type: SensManf'}
-                    />
-                }
-                child5={
-                    <Sensor 
-                        page={'Line'}
-                        info={'Sensor: Temperature \n Type: SensManf'}
-                        faceNorthEast={true}
-                    />
-                }
+                // child2={
+                //     <Sensor 
+                //         page={'Line'} 
+                //         info={'Sensor: CO2 \n Type: ABC Sensor \n Graph: Line'}
+                //     />
+                // }
+                // child5={
+                //     <Sensor 
+                //         page={'Line'}
+                //         info={'Sensor: Temperature \n Type: ABC Sensor \n Graph: Line'}
+                //         faceNorthEast={true}
+                //     />
+                // }
             />
             {/* col 7 */}
             <Columnx
                 child3={
                     <Sensor 
-                        page={'Line'} 
-                        info={'Sensor: CO \n Type: SensManf'}
+                        page={'Gauge'} 
+                        info={'Sensor: CO \n Type: ABC Sensor \n Graph: Gauge'}
                         faceNorthEast={true}
                     />
                 }
                 child6={
                     <Sensor 
                         page={'Line'}
-                        info={'Sensor: Temperature \n Type: SensManf'}
+                        info={'Sensor: Temperature \n Type: ABC Sensor \n Graph: Line'}
                         faceNorthEast={true}
                     />
                 }
             />
             {/* col 8 */}
             <Columnx
-                child3={
-                    <Sensor 
-                        page={'Line'} 
-                        info={'Sensor: CO2 \n Type: SensManf'}
-                        faceNorthEast={true}
-                    />
-                }
-                child6={
-                    <Sensor 
-                        page={'Line'}
-                        info={'Sensor: Temperature \n Type: SensManf'}
-                        faceNorthEast={true}
-                    />
-                }
+                // child3={
+                //     <Sensor 
+                //         page={'Line'} 
+                //         info={'Sensor: CO2 \n Type: ABC Sensor \n Graph: Line'}
+                //         faceNorthEast={true}
+                //     />
+                // }
+                // child6={
+                //     <Sensor 
+                //         page={'Line'}
+                //         info={'Sensor: Temperature \n Type: ABC Sensor \n Graph: Line'}
+                //         faceNorthEast={true}
+                //     />
+                // }
             />
             {/* col 9 */}
             <Columnx
                 child4={
                     <Sensor 
-                        page={'Line'} 
-                        info={'Sensor: Temperature \n Type: SensManf'}
+                        page={'Gauge'} 
+                        info={'Sensor: Temperature \n Type: ABC Sensor \n Graph: Gauge'}
                         faceNorthEast={true}
                     />
                 }
                 child6={
                     <Sensor 
                         page={'Line'}
-                        info={'Sensor: Temperature \n Type: SensManf'}
+                        info={'Sensor: Temperature \n Type: ABC Sensor \n Graph: Line'}
                         faceNorthEast={true}
                     />
                 }
             />
             {/* col 10 */}
             <Columnx
-                child4={
-                    <Sensor 
-                        page={'Line'} 
-                        info={'Sensor: CO2 \n Type: SensManf'}
-                        faceNorthEast={true}
-                    />
-                }
-                child5={
-                    <Sensor 
-                        page={'Line'}
-                        info={'Sensor: CO2 \n Type: SensManf'}
-                        faceNorthEast={true}
-                    />
-                }
-                child6={
-                    <Sensor 
-                        page={'Bar'}
-                        info={'Sensor: CO2 \n Type: SensManf'}
-                        faceNorthEast={true}
-                    />
-                }
+                // child4={
+                //     <Sensor 
+                //         page={'Line'} 
+                //         info={'Sensor: CO2 \n Type: ABC Sensor \n Graph: Line'}
+                //         faceNorthEast={true}
+                //     />
+                // }
+                // child5={
+                //     <Sensor 
+                //         page={'Line'}
+                //         info={'Sensor: CO2 \n Type: ABC Sensor \n Graph: Line'}
+                //         faceNorthEast={true}
+                //     />
+                // }
+                // child6={
+                //     <Sensor 
+                //         page={'Bar'}
+                //         info={'Sensor: CO2 \n Type: ABC Sensor \n Graph: Bar'}
+                //         faceNorthEast={true}
+                //     />
+                // }
             />
         </div>
     );
